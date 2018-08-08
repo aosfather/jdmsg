b87cef31926225443e7d52938303231f7879b675
aosfather <aosfather@163.com>
2018-08-08 16:58:08 +0800

    add the config file

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import "github.com/aosfather/bingo"
 
 func main() {
-	app:=bingo.TApplication{}
-    app.SetHandler(nil,loadControl)
-	app.RunApp()
+	app := bingo.TApplication{}
+	app.SetHandler(nil, loadControl)
+	app.Run("config.conf")
 }
 
-func loadControl(mvc *bingo.MvcEngine,context *bingo.ApplicationContext) bool{
+func loadControl(mvc *bingo.MvcEngine, context *bingo.ApplicationContext) bool {
 
 	mvc.AddController(&AlphaController{})
 	return true
