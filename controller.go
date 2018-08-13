2fb7edd31d5b3ec65883f9a82e3c63d8cb5935b0
aosfather <aosfather@163.com>
2018-08-13 11:54:49 +0800

    完成几个的交互功能。

--- controller.go
+++ controller.go
@@ -33,10 +33,71 @@ func (this *AlphaController) Post(c mvc.Context, p interface{}) (interface{}, mv
 		res.Version = "1.0"
 		res.ShouldEnd = true
 		res.Response.Output.Type = "PlainText"
-		res.Response.Output.Text = "欢迎使用！"
+		if value.Request.Type == "LaunchRequest" {
 
-		this.Skill.GetMessages("111")
+			res.Response.Output.Text = "欢迎使用我的留言！"
+
+		} else if value.Request.Type == "IntentRequest" {
+			name := value.Request.Intent.Name
+			switch name {
+			//绑定手机
+			case "bind_mobile":
+				res.Response.Output.Text = this.bindMobile(value.Session.Device.Id, value.Request.Intent)
+			case "get_msg":
+				res.Response.Output.Text = this.getMessages(value.Session.Device.Id)
+			case "send_msg":
+				res.Response.Output.Text = this.sendMessage(value.Session.Device.Id, value.Request.Intent)
+			}
+
+		}
 		return &res, nil
 	}
 	return nil, nil
+}
+
+//绑定手机
+func (this *AlphaController) bindMobile(deviId string, intent _intent) string {
+	slot := intent.Slots["mobile"]
+	if slot.ConfirmResult == "CONFIRMED" && slot.Matched {
+		mobile := slot.Value
+		if this.Skill.CreateAlias(mobile, deviId) {
+			return "绑定成功"
+		}
+
+	}
+
+	return "绑定失败，请重新绑定，并确认手机号输入正确，未被其它设备绑定！"
+
+}
+
+//获取留言
+func (this *AlphaController) getMessages(deviId string) string {
+	msgs := this.Skill.GetMessages(deviId)
+	if len(msgs) > 0 {
+		rmsg := fmt.Sprintf("您有%d条留言。", len(msgs))
+
+		for index, msg := range msgs {
+			rmsg += fmt.Sprintf("第%d条:%s。", index+1, msg)
+		}
+
+		return rmsg
+
+	}
+
+	return "您没有新的留言！"
+}
+
+//发送留言
+func (this *AlphaController) sendMessage(devid string, intent _intent) string {
+	target := intent.Slots["target"]
+	msg := intent.Slots["values"]
+
+	if this.Skill.GetDeviceIdByAlias(target.Value) != "" {
+
+		this.Skill.SendMessage(devid, target.Value, msg.Value)
+
+		return fmt.Sprintf("给%s留言成功！", target.Value)
+	}
+
+	return fmt.Sprintf("留言失败，%s不存在！", target.Value)
 }
